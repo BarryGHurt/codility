4924c1a1247033721de019edfb4add647986d124
BarryGHurt <barry.hurt@protonmail.com>
2020-10-10 18:07:55 -0700

    Permutation Missing Element

---
+++ Codility/TimeComplexity.cs
@@ -0,0 +1,47 @@
+﻿using System;
+namespace Codility
+{
+    public class TimeComplexity
+    {
+        public TimeComplexity()
+        {
+
+        }
+
+        public int PermMissingElement(int[] A)
+        {
+            // Find the missing element in a given permutation.
+            int result = 0;
+
+            switch (A.Length)
+            {
+                case 0:
+                    result = 1;
+                    break;
+                case 1:
+                    result = A[0] == 1 ? 2 : A[0] - 1;
+                    break;
+                default:
+                    Array.Sort(A);
+
+                    if (A[0] > 1)
+                    {
+                        result = 1;
+                        break;
+                    }
+
+                    for (int i = 0; i < A.Length; i++)
+                    {
+                        if (i + 1 == A.Length || A[i + 1] - A[i] == 2)
+                        {
+                            result = A[i] + 1;
+                            break;
+                        }
+                    }
+                    break;
+            }
+
+            return result;
+        }
+    }
+}
--- CodilityTests/ProblemTests.cs
+++ CodilityTests/ProblemTests.cs
@@ -48,5 +48,26 @@ namespace CodilityTests
             Assert.AreEqual(11, testout2);
 
         }
+
+        [Test]
+        public void PermMissingElementTest()
+        {
+            var timeComp = new TimeComplexity();
+
+            int[] test = { 2, 3, 1, 5 };
+
+            int result = timeComp.PermMissingElement(test);
+
+            Assert.AreEqual(4, result);
+
+            int[] test2 = { 3, 2 };
+
+            int result2 = timeComp.PermMissingElement(test2);
+
+            Assert.AreEqual(1, result2);
+
+
+
+        }
     }
 }
